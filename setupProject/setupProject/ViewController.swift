d7f98464a3d7c76dd51e1629592bd453078bb53c
Mikhail <55313671+Drozdnik@users.noreply.github.com>
2024-01-02 00:23:19 +0300

    setupProject

---
+++ setupProject/setupProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  setupProject
+//
+//  Created by Михаил  on 31.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

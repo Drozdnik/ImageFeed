56d85388e716db0f67a5029b69984b9b57f0f8c5
Drozdnik <drozdnik@list.ru>
2024-01-08 12:55:09 +0300

    view label ImageList

--- setupProject/setupProject/ImagesListViewController.swift
+++ setupProject/setupProject/ImagesListViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ImagesListViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+}
+
+extension ImagesListViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}

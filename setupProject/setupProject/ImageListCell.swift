b9d1e7b840a1ea02d9e237d045a1e12d531666f4
Drozdnik <drozdnik@list.ru>
2024-01-13 20:46:00 +0300

    Add images arr, aand DateFormatter

--- setupProject/setupProject/ImageListCell.swift
+++ setupProject/setupProject/ImageListCell.swift
@@ -2,6 +2,10 @@
 import UIKit
 
 final class ImageListCell: UITableViewCell{
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var cellImage: UIImageView!
+    
     static let resuceIdentifier = "ImageListCell"
     
 }
--- setupProject/setupProject/ImagesListViewController.swift
+++ setupProject/setupProject/ImagesListViewController.swift
@@ -10,22 +10,35 @@ import UIKit
 class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
+    private let photosName: [String] = Array(0..<20).map{"\($0)"}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top:12, left: 0, bottom: 12, right: 0)
         tableView.delegate = self
         tableView.dataSource = self
-
     }
-
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
 }
 
 extension ImagesListViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    private func tableView(_ tableView:UITableView, didSelectRowAt indexPath: IndexPath) -> CGFloat{
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+    }
 }
 
 extension ImagesListViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -37,9 +50,11 @@ extension ImagesListViewController: UITableViewDataSource{
         
         return imageListCell
     }
-    
 }
+
 extension ImagesListViewController{
-    func configCell(for cell: ImageListCell, with indexPath: IndexPath) { }
+    func configCell(for cell: ImageListCell, with indexPath: IndexPath) {
+        dateFormatter.string(from: <#T##Date#>)
+    }
     
 }

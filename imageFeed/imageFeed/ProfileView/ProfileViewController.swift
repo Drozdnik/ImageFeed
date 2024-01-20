d9f84939a320b7c3986809b89b5345a333cfee81
Drozdnik <drozdnik@list.ru>
2024-01-20 18:49:22 +0300

    Add IBOutlets and IBAction

--- imageFeed/imageFeed/ProfileView/ProfileViewController.swift
+++ imageFeed/imageFeed/ProfileView/ProfileViewController.swift
@@ -1,5 +1,12 @@
 import UIKit
 
 class ProfileViewController: UIViewController{
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var loginNameLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var logOutButton: UIButton!
+    @IBOutlet var avatarImage: UIImageView!
     
+    @IBAction func didTapLogoutButton(_ sender: UIButton) {
+    }
 }

80ced05da0591f5435c28e70cec4e9ebf2211490
Drozdnik <drozdnik@list.ru>
2024-01-20 17:55:17 +0300

    Rename main folder

---
+++ imageFeed/imageFeed/ImageListCell.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+final class ImageListCell: UITableViewCell{
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var cellImage: UIImageView!
+    
+    static let resuceIdentifier = "ImageListCell"
+    
+}
---
+++ imageFeed/imageFeed/ImagesListViewController.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+    
+    private let photosName: [String] = Array(0..<20).map{"\($0)"}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.sectionHeaderHeight = 300
+    }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+}
+
+extension ImagesListViewController: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+         return photosName.count
+     }
+     
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return 1 // Теперь каждая секция будет содержать только одну ячейку
+     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImageListCell.resuceIdentifier, for: indexPath)
+        guard let imageListCell = cell as? ImageListCell else {
+            return UITableViewCell()
+        }
+        configCell(for: imageListCell, with: indexPath)
+        
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.section]) else {
+            return 0
+        }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 10 // Высота отступа между секциями
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+          let footerView = UIView()
+          footerView.backgroundColor = UIColor.clear // Сделать фон прозрачным
+          return footerView
+      }
+}
+
+extension ImagesListViewController{
+    func configCell(for cell: ImageListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.section]) else {
+            return
+        }
+        
+        cell.cellImage.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        let isLiked = indexPath.section % 2  == 1
+        let likeImage = isLiked ? UIImage(named: "buttonTapped") : UIImage(named: "buttonDisabled")
+        cell.likeButton.setImage(likeImage, for: .normal)
+    }
+}

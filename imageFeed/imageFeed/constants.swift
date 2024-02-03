e59576dfe39a855a08aa76bb96cf53b39d203955
Drozdnik <drozdnik@list.ru>
2024-02-03 23:17:40 +0300

    Deleted extra .gitignore, added WebView

---
+++ imageFeed/imageFeed/constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let accessKey: String = "TTxNmP3O3DhG3TyWrnQs_Ry_fXjSSExAI6WIe8WmENw"
+let secterKet: String = "PU9_PPg5qpOgAVsMog9TqURjhiobKqjCdvozO5vUxvM"
+let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+let acessScope: String = "public+read_user+write_likes"
+let defaultBaseUrl = URL(string: "https://api.unsplash.com")

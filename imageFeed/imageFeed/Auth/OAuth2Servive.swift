9cfd5e59c40c0b1f5cf882558f58d2724d8c31a1
Drozdnik <drozdnik@list.ru>
2024-02-06 22:17:34 +0300

    Deleted force Unwrap

--- imageFeed/imageFeed/Auth/OAuth2Servive.swift
+++ imageFeed/imageFeed/Auth/OAuth2Servive.swift
@@ -15,27 +15,28 @@ final class OAuth2Service{
             OAuth2TokenStorage().token = newValue
         }
     }
-
-        func fetchOAuthToken(
-            _ code: String,
-    completion: @escaping (Result<String, Error>) -> Void ){
-            let request = authTokenRequest(code: code)
-            let task = object(for: request) { [weak self] result in
-                guard let self = self else { return }
-                switch result {
-                case .success(let body):
-                    let authToken = body.accessToken
-                    self.authToken = authToken
-                    completion(.success(authToken))
-                case .failure(let error):
-                    completion(.failure(error))
+    
+    func fetchOAuthToken(
+        _ code: String,
+        completion: @escaping (Result<String, Error>) -> Void ){
+            if let request = authTokenRequest(code: code){
+                let task = object(for: request) { [weak self] result in
+                    guard let self = self else { return }
+                    switch result {
+                    case .success(let body):
+                        let authToken = body.accessToken
+                        self.authToken = authToken
+                        completion(.success(authToken))
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
                 }
-                
+                task.resume()
+            } else {
+                assertionFailure("Request failed")
             }
-            task.resume()
         }
-    }
-
+}
     extension OAuth2Service {
         private func object(
             for request: URLRequest,
@@ -50,17 +51,15 @@ final class OAuth2Service{
             }
     }
         
-        private func authTokenRequest(code: String) -> URLRequest {
-            URLRequest.makeHTTPRequest(
-                path: "/oauth/token"
-                + "?client_id=\(accessKey)"
-                + "&&client_secret=\(secretKey)"
-                + "&&redirect_uri=\(redirectURI)"
-                + "&&code=\(code)"
-                + "&&grant_type=authorization_code",
-                httpMethod: "POST",
-                baseURL: URL(string: "https://unsplash.com")!
-    ) }
+        private func authTokenRequest(code: String) -> URLRequest? {
+                let path = "/oauth/token"
+            + "?client_id=\(accessKey)"
+            + "&&client_secret=\(secretKey)"
+            + "&&redirect_uri=\(redirectURI)"
+            + "&&code=\(code)"
+            + "&&grant_type=authorization_code"
+            return URLRequest.makeHTTPRequest(path: path, httpMethod: "POST", baseURL: defaultBaseURL)
+    }
         
         private struct OAuthTokenResponseBody: Decodable {
             let accessToken: String

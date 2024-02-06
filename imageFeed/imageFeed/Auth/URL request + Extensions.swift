058e507d3a0495c59b5a3779895e74e2a0c602b2
Drozdnik <drozdnik@list.ru>
2024-02-06 23:18:33 +0300

    URL request + extension

---
+++ imageFeed/imageFeed/Auth/URL request + Extensions.swift
@@ -0,0 +1,13 @@
+
+import WebKit
+
+extension URLRequest {
+    static func makeHTTPRequest(
+        path: String,
+        httpMethod: String,
+        baseURL: URL = defaultBaseURL
+    ) -> URLRequest {
+        var request = URLRequest(url: URL(string: path, relativeTo: baseURL)!)
+               request.httpMethod = httpMethod
+               return request
+       } }
--- imageFeed/imageFeed/Constants.swift
+++ imageFeed/imageFeed/Constants.swift
@@ -5,3 +5,4 @@ let secretKey: String = "PU9_PPg5qpOgAVsMog9TqURjhiobKqjCdvozO5vUxvM"
 let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
 let acessScope: String = "public+read_user+write_likes"
 let defaultBaseUrl = URL(string: "https://api.unsplash.com")
+let defaultBaseURL = URL(string: "https://api.unsplash.com")!

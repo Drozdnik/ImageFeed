83490c420b7d3cac1fd83384930167c8db81e5c3
Drozdnik <drozdnik@list.ru>
2024-03-07 00:09:34 +0300

    Added ImageListService and ImageListModel

---
+++ imageFeed/imageFeed/ImageListModel.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Photo {
+    let id: String
+    let size: CGSize
+    let createdAt: Date?
+    let welcomeDesctiption: String?
+    let thumbImageURL: String
+    let largeImageURL: String
+    let isLiked: Bool
+}
---
+++ imageFeed/imageFeed/imageListService.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class ImageListService {
+    
+    private lazy var lastLoadedPage: Int = 0
+    private (set) var photos: [Photo] = []
+    
+    func fetchProtosNextPage() {
+        
+    }
+}

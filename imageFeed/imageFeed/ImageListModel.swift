6711b2559218a93a9c1278fa21438d628c77397f
Drozdnik <drozdnik@list.ru>
2024-03-07 00:45:17 +0300

    Added structs: Decodable for JSON

--- imageFeed/imageFeed/ImageListModel.swift
+++ imageFeed/imageFeed/ImageListModel.swift
@@ -9,3 +9,25 @@ struct Photo {
     let largeImageURL: String
     let isLiked: Bool
 }
+
+struct PhotoResult: Decodable{
+    let id: String
+    let width: Int
+    let height: Int
+    let createdAt: Date?
+    let description: String?
+    let likedByUser: Bool
+    let urls: URlImageResult
+}
+
+struct URlImageResult:Decodable{
+    let thumb: String
+    let full: String
+}
+
+// ToDo
+let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    return formatter
+}()

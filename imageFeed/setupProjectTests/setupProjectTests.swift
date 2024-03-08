aa3d1f8d3e7ea332315d6f34afd3cdece83b0fea
Drozdnik <drozdnik@list.ru>
2024-03-08 15:41:28 +0300

    Refactor Test

--- imageFeed/setupProjectTests/setupProjectTests.swift
+++ imageFeed/setupProjectTests/setupProjectTests.swift
@@ -8,44 +8,6 @@
 import XCTest
 @testable import imageFeed
 
-    
-//final class ImageListServiceTest: XCTestCase{
-//    func testImageFetch(){
-//        let service = ImageListService()
-//        var resultPhotos: [Photo]?
-//        var resultError: Error?
-//        let expectation = self.expectation(description: "Wait for Notification")
-//        NotificationCenter.default.addObserver(
-//            forName: ImageListService.didChangeNotification,
-//            object: nil,
-//            queue: .main) { _ in
-//                expectation.fulfill()
-//            }
-//
-//        service.fetchPhotosNextPage{result in
-//            switch result{
-//            case .success(let photos):
-//                resultPhotos = photos
-//            case .failure(let error):
-//                resultError = error
-//            }
-//            expectation.fulfill()
-//        }
-//        wait(for: [expectation], timeout: 10)
-//
-//        XCTAssertEqual(service.photos.count, 10)
-//
-//        XCTAssertNotNil(resultPhotos)
-//        XCTAssertNil(resultError)
-//
-//    }
-//
-//
-//    func testExample() throws {
-//        testImageFetch()
-//    }
-//}
-
 final class ImageListServiceTest: XCTestCase {
     func testFetchPhotosNextPage() {
         let service = ImageListService()
